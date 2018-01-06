055e0a49b00b47fcca78c66cdff4c15e3410237d
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-01-06 23:15:59 +0500

    Two Routes added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 import Routes from "./routes/index";
 import "./database";
+import bodyParser from "body-parser";
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Setup Routes
 app.use(Routes);
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,24 @@ import express from "express";
 import Todo from "../models/todos";
 const Router = express.Router();
 
-Router.get("/", (req, res) => {
-    res.send("Hello World");
+Router.post("/todo", (req, res) => {
+    const newTodo = new Todo({ txt: req.body.txt });
+    newTodo.save((err) => {
+        if (err) { res.json({ err: err }); }
+        else {
+            res.json({ status: true });
+        }
+    });
+});
+
+Router.get("/allTodos", (req, res) => {
+    Todo.find((err, data) => {
+        if (err) {
+            res.json({ err });
+        } else {
+            res.json({ status: true, data: data });
+        }
+    });
 });
 
 module.exports = Router;
